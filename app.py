9a6941d2330d96d0b0d35a9cbe1482fb69b2aebf
Nathan Moderwell <nathan.moderwell@gmail.com>
2021-10-16 18:27:10 -0700

    add text parsing

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import re
 
 app = Flask(__name__)
 
@@ -11,7 +12,12 @@ def homepage():
 
 @app.route('/scheduleme', methods=['POST'])
 def scheduleme():
-    return 'I would like to schedule..'
+    raw_text = request.form.get('text')
+    text_array = re.findall(r'"(.*?)"', raw_text)
+    if len(text_array) != 3: 
+        return 'The format is /scheduleme "[title]" "[start date & time]" "[end date & time]"'
+    title, start, end = text_array 
+    return 'Sweet I parsed the title: {}, start: {} and end: {}'.format(title, start, end)
 
 if __name__ == '__main__':
     app.run(debug=True, port=PORT)
